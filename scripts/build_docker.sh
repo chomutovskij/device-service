0c88cc2776b499a08e327ba53a568ee7a302d637
Andrej Chomutovskij <andrej.chomutovskij@gmail.com>
2023-09-12 02:28:05 +0400

    fix docker script

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -9,12 +9,15 @@ rm -rf $DEST
 mkdir -p $DEST/device-service-server/var/conf
 mkdir -p $DEST/device-service-server/var/db
 mkdir -p $DEST/device-service-server/var/gsmarena_data
+mkdir -p $DEST/device-service-server/var/certs
 
 cp ./scripts/Dockerfile $DEST/
 tar -xf "./device-service-server/build/distributions/device-service-server-${VERSION}.tar" -C $DEST/device-service-server --strip-components=1
 cp ./device-service-server/var/conf/conf.yml $DEST/device-service-server/var/conf
 cp ./device-service-server/var/db/database.db $DEST/device-service-server/var/db
 cp ./device-service-server/var/gsmarena_data/gsmarena_dataset.csv $DEST/device-service-server/var/gsmarena_data
+cp ./device-service-server/var/certs/keystore.jks $DEST/device-service-server/var/certs
+cp ./device-service-server/var/certs/truststore.jks $DEST/device-service-server/var/certs
 
 cd $DEST
 docker build -t "chomutovskij/device-service-server:$VERSION" .
